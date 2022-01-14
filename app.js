a42aa46835a9963781750f1e9a69effe8e1c5eef
RasmusMoller <rasmus.moller@se.experis.com>
2022-01-14 16:43:59 +0100

    FEAT: Added laptop menu with feature text and but function

--- app.js
+++ app.js
@@ -6,12 +6,85 @@ const payElement = document.getElementById('pay-balance');
 const bankBtnElement = document.getElementById('bank-btn');
 const workBtnElement = document.getElementById('work-btn');
 const repayBtnElement = document.getElementById('repay-btn');
+const laptopMenuElement = document.getElementById('laptop-menu');
+const specListElement = document.getElementById('spec-list');
+const specTextBoxElement = document.getElementById('spec-text-box');
+const laptopImgElement = document.getElementById('laptop-img');
+const laptopPriceElement = document.getElementById('laptop-price');
+const laptopTitleElement = document.getElementById('laptop-title');
+const laptopInfoElement = document.getElementById('laptop-info');
+const buyBtnElement = document.getElementById('buy-btn');
+const buyErrorTextElement = document.getElementById('buy-error-text');
 
 let balance = 10;
 let loan = 0;
 let pay = 0;
-const activeLoanMsg  = "\n You already have an active loan. Please pay it back before taking another.";
+let laptops = [];
+let laptopImgList = [];
+const activeLoanMsg  = "You already have an active loan. Please pay it back before taking another.";
 const errorMsg = document.createElement('span');
+
+// Fetches data from API and adds to list of laptops
+fetch('https://noroff-komputer-store-api.herokuapp.com/computers')
+  .then(response => response.json())
+  .then(data => laptops = data)
+  .then(laptops => addLaptopsToMenu(laptops))
+  .catch(error => console.log(error));
+
+const addLaptopsToMenu = (laptopList) => {
+  laptopList.forEach(e => addLaptopToMenu(e));
+  handleLaptopMenuChange();
+}
+// Add a laptop option element to the select dropdown menu
+const addLaptopToMenu = (laptop) => {
+  const laptopOption = new Option(laptop.title, laptop.id);
+  laptopMenuElement.appendChild(laptopOption);
+  laptopImgList.push(laptop.image);
+}
+// Changes all laptop info elements when changing option in the laptop select menu
+const handleLaptopMenuChange = () => {
+  const laptopSpecs = GetCurrentlySelectedLaptop().specs;
+  specListElement.innerHTML = ""; // Remove list elements from spec list before adding new
+
+  // Add feature list
+  addListItemsWithText(laptopSpecs, specListElement);
+  changeLaptopImgSrc();
+  //TODO: Change laptop info
+  ChangeLaptopInfo();
+  hideElements([buyErrorTextElement]);
+
+  //TODO: Change laptop price
+}
+
+function addListItemsWithText(dataList, listElement){
+  for(const item of dataList){
+    const itemElement = document.createElement('li');
+    itemElement.innerText = item;
+    listElement.appendChild(itemElement);
+  }
+}
+function changeLaptopImgSrc(){
+  const imgSrc = "https://noroff-komputer-store-api.herokuapp.com/" + laptopImgList[GetCurrentlySelectedLaptop().id - 1];
+  laptopImgElement.src = imgSrc;
+  document.getElementById('img-error-text').style.visibility = "hidden";
+}
+laptopImgElement.onerror = function(){
+  this.onerror = null;
+  this.src = 'sadge-kitten.jpg';
+  document.getElementById('img-error-text').style.visibility = "visible";
+}
+function ChangeLaptopInfo(){
+  currentSelectedLaptop = GetCurrentlySelectedLaptop();
+  laptopTitleElement.innerText = currentSelectedLaptop.title;
+  laptopInfoElement.innerText = currentSelectedLaptop.description;
+  laptopPriceElement.innerText = formatNumToSEK(currentSelectedLaptop.price);
+}
+
+const GetCurrentlySelectedLaptop = () => {
+  return laptops[laptopMenuElement.selectedIndex];
+}
+
+laptopMenuElement.addEventListener('change', handleLaptopMenuChange);
 
 // We can only loan up to double the amount of our balance
 const handleLoan = () => {
@@ -36,11 +109,11 @@ const handleLoan = () => {
       balance += loanAmount;
       loan += loanAmount;
       }
+    else return;
   }while(loan <= 0); // Loops until user input is valid
   
   updateBank();
 }
-
 const handleBank = () => {
   loanPayment = pay * 0.1;
   balanceAmount = pay * 0.9;
@@ -57,11 +130,9 @@ const handleBank = () => {
   }
   pay = 0;
 }
-
 const handleWork = () => {
   pay += 100;
 }
-
 const handleRepay = () => {
   if(loan > pay){
     loan -= pay;
@@ -72,24 +143,23 @@ const handleRepay = () => {
     loan = 0;
   }
 }
-
-loanBtnElement.addEventListener('click', () => {handleLoan(); updateBank();});
-bankBtnElement.addEventListener('click', () => {handleBank(); updateBank();});
-workBtnElement.addEventListener('click', () => {handleWork(); updateBank();});
-repayBtnElement.addEventListener('click', () => {handleRepay(); updateBank();});
-
 function updateBank(){
   if(loan === 0){
-    hideElements([loanTextElement, errorMsg]);
+    hideElements([loanTextElement, errorMsg, repayBtnElement]);
   }
   else{
     loanElement.innerText = formatNumToSEK(loan);
-    showElements([loanTextElement, errorMsg]);
+    showElements([loanTextElement, errorMsg, repayBtnElement]);
   }
 
   balanceElement.innerText = formatNumToSEK(balance);
   payElement.innerText = formatNumToSEK(pay);
 }
+
+loanBtnElement.addEventListener('click', () => {handleLoan(); updateBank();});
+bankBtnElement.addEventListener('click', () => {handleBank(); updateBank();});
+workBtnElement.addEventListener('click', () => {handleWork(); updateBank();});
+repayBtnElement.addEventListener('click', () => {handleRepay(); updateBank();});
 
 function hideElements(elementsList){
   elementsList.forEach(e => e.style.visibility = 'hidden');
@@ -97,11 +167,22 @@ function hideElements(elementsList){
 function showElements(elementsList){
   elementsList.forEach(e => e.style.visibility = 'visible');
 }
-
 const formatNumToSEK = (number) => {
   return new Intl.NumberFormat(
     'sv-SE', {style: 'currency', currency: 'SEK' })
     .format(number);
 }
 
+const handleBuy = () => {
+  const priceToPay = parseInt(GetCurrentlySelectedLaptop().price);
+  if(balance > priceToPay){
+    balance -= priceToPay;
+    alert("Successfully bought laptop!");
+  }
+  else{
+    showElements([buyErrorTextElement]);
+  }
+}
+
+buyBtnElement.addEventListener('click', () => {handleBuy(); updateBank();});
 updateBank();
